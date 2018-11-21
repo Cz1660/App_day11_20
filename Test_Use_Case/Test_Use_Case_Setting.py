878c64ab834e42dcbfdb107ad6f0ad1bb3b401e7
Your Name <you@example.com>
2018-11-21 12:40:49 +0800

    'shangchuan007'

--- Test_Use_Case/Test_Use_Case_Setting.py
+++ Test_Use_Case/Test_Use_Case_Setting.py
@@ -11,7 +11,7 @@ class Test_Login:
     def test_setting(self):
         for i in Page.input_list:
             self.Dv.return_page().send_keys_text(Page.search_setting,i)
-            allure.attach('输入',i)
+            allure.attach('输入','i')
             self.Dv.return_page().gain_elements_text()
             assert i in self.Dv.return_page().gain_elements_text()
 
