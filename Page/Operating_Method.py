2ff5fc61baa73450e32eb7319d7a62f3b751e5ce
Your Name <you@example.com>
2018-11-21 14:22:34 +0800

    'shangchuan012'

--- Page/Operating_Method.py
+++ Page/Operating_Method.py
@@ -4,12 +4,15 @@ class Operating_Method(Base_Method):
     def __init__(self,driver):
         Base_Method.__init__(self,driver)
     # 获取一组元素的text
-    @allure.step('获取这一组元素的text值')
+    # @allure.step('获取这一组元素的text值')
     def gain_elements_text(self,loc):
         list = []
         elements = self.find_elements(loc)
         for i in elements:
             list.append(i.text)
-        allure.attach('描述',list)
         return list
 
+    @allure.step('获取这一组元素的text值')
+    def gain_list(self,loc):
+        list = self.gain_elements_text(loc)
+        allure.attach('描述',list)
