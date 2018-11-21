74dc6b373f9c129c612e52af25bc0faf89398eb6
Your Name <you@example.com>
2018-11-21 13:43:58 +0800

    'shangchuan012'

--- Page/Operating_Method.py
+++ Page/Operating_Method.py
@@ -10,5 +10,5 @@ class Operating_Method(Base_Method):
         elements = self.find_elements(loc)
         for i in elements:
             list.append(i.text)
-        allure.attach('text值',title =list)
-        return list
+
+        return list,allure.attach('text值',list)
