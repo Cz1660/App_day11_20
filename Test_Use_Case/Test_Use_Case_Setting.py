60ce290edc7bde9fa79f579098c29467bce59718
Your Name <you@example.com>
2018-11-21 10:10:23 +0800

    'shangchuan_001'

--- Test_Use_Case/Test_Use_Case_Setting.py
+++ Test_Use_Case/Test_Use_Case_Setting.py
@@ -1,6 +1,6 @@
 from Base.Get_Driver import Get_Driver
 from Page.Return_Page import Return_Page
-import Page
+import Page,allure
 class Test_Login:
     def setup_class(self):
         self.Dv = Return_Page(Get_Driver().get_driver())
@@ -9,3 +9,5 @@ class Test_Login:
     def test_setting(self):
         for i in Page.input_list:
             self.Dv.return_page().send_keys_text(Page.search_setting,i)
+    def test_setting_001(self):
+        self.Dv.return_page().send_keys_text(Page.search_setting,i)
