d0ae2f1a58a6176f0b1c15384daf9b4b936de790
Your Name <you@example.com>
2018-11-21 13:33:11 +0800

    'shangchuan012'

--- Page/__init__.py
+++ Page/__init__.py
@@ -2,4 +2,4 @@ from selenium.webdriver.common.by import By
 
 search_setting = (By.ID,'android:id/search_src_text')
 search_result = (By.ID,'com.android.settings:id/title')
-input_list = [['移动网络'],['电池']]
+input_list = ['移动网络','电池']
